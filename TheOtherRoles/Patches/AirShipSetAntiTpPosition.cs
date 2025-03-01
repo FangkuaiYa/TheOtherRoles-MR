264958262ab7a0cefe8ceb8bdd66fddbb19516f9
MonsoonJiFeng <1767265134@qq.com>
2025-03-01 10:59:07 +0800

    Save the position of the player prior to starting the climb / gap platform.

---
+++ TheOtherRoles/Patches/AirShipSetAntiTpPosition.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+
+namespace TheOtherRoles.Patches
+{
+    [HarmonyPatch]
+    public static class AirShipSetAntiTpPosition
+    {
+
+        // Save the position of the player prior to starting the climb / gap platform
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerPhysics), nameof(PlayerPhysics.ClimbLadder))]
+        public static void prefix()
+        {
+            AntiTeleport.position = PlayerControl.LocalPlayer.transform.position;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MovingPlatformBehaviour), nameof(MovingPlatformBehaviour.UsePlatform))]
+        public static void prefix2()
+        {
+            AntiTeleport.position = PlayerControl.LocalPlayer.transform.position;
+        }
+    }
+}
